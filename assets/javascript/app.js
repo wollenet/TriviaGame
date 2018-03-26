a1ca55e01ed443a90557020c0245b8f465552476
wollenet <ewollenberg16@jcu.edu>
2018-03-26 15:52:13 -0400

    pushing new updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,184 @@
-function populate() {
-	if(quiz.isEnded()) {
-		showscores();
-	}
-	else {
-		// show question
-		var element = document.getElementById("question");
-		element.innerHTML = quiz.getQuestionIndex().text;
 
-		// show choices
-		var choices = quiz.getQuestionIndex().choices;
-		for (var i = 0; i < choices.length; i++) {
-			var element = document.getElementById("choice" + i);
-			element.innerHTML = choice[i];
-			guess("btn" + i, choices[i]);
-		}
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
-		showProgress();
-	}
+// Question set
+var questions = [{
+  question: "What was the first full length CGI movie?",
+  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+  image: "assets/images/toystory.gif"
+}, {
+  question: "Which of these is NOT a name of one of the Spice Girls?",
+  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+  correctAnswer: "Fred Spice",
+  image: "assets/images/spicegirls.gif"
+}, {
+  question: "Which NBA team won the most titles in the 90s?",
+  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+  correctAnswer: "Chicago Bulls",
+  image: "assets/images/bulls.gif"
+}, {
+  question: "Which group released the hit song, 'Smells Like Teen Spirit'?",
+  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+  correctAnswer: "Nirvana",
+  image: "assets/images/nirvanabark.gif"
+}, {
+  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+  correctAnswer: "The Lion King",
+  image: "assets/images/lionking.gif"
+}, {
+  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
+  answers: ["Dice", "Mirror", "Fresh", "Cab"],
+  correctAnswer: "Fresh",
+  image: "assets/images/fresh.gif"
+}, {
+  question: "What was Doug's best friend's name?",
+  answers: ["Skeeter", "Mark", "Zach", "Cody"],
+  correctAnswer: "Skeeter",
+  image: "assets/images/skeeter.gif"
+}, {
+  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+  correctAnswer: "Mr.Belding",
+  image: "assets/images/belding.gif"
+}];
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
+
+  countdown: function() {
+    this.counter--;
+    $("#counter-number").text(this.counter);
+    if (this.counter === 0) {
+      console.log("TIME UP");
+      this.timeUp();
+    }
+  },
+
+  loadQuestion: function() {
+
+    timer = setInterval(this.countdown.bind(this), 1000);
+
+    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+
+    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    }
+  },
+
+  nextQuestion: function() {
+    this.counter = window.countStartNumber;
+    $("#counter-number").text(this.counter);
+    this.currentQuestion++;
+    this.loadQuestion.bind(this)();
+  },
+
+  timeUp: function() {
+
+    clearInterval(window.timer);
+
+    $("#counter-number").text(this.counter);
+
+    panel.html("<h2>Out of Time!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results, 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion, 3 * 1000);
+    }
+  },
+
+  results: function() {
+
+    clearInterval(window.timer);
+
+    panel.html("<h2>All done, heres how you did!</h2>");
+
+    $("#counter-number").text(this.counter);
+
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+    panel.append("<br><button id='start-over'>Start Over?</button>");
+  },
+
+  clicked: function(e) {
+    clearInterval(window.timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+  answeredIncorrectly: function() {
+
+    this.incorrect++;
+
+    clearInterval(window.timer);
+
+    panel.html("<h2>Nope!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results.bind(this), 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+    }
+  },
+
+  answeredCorrectly: function() {
+
+    clearInterval(window.timer);
+
+    this.correct++;
+
+    panel.html("<h2>Correct!</h2>");
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results.bind(this), 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+    }
+  },
+
+  reset: function() {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
 };
 
-function guess(id, guess) {
-	var button = document.getElementById(id);
-	button.onclick = function() {
-		quiz.guess(guess);
-		populate();
-	}
-};
+// CLICK EVENTS
 
-function showProgress() {
-	var currentQuestionNumber = quiz.questionsIndex + 1;
-	var element = documnet.getElementById("progress");
-	element.innerHTML = "question" + currentQuestionNumber + "of" + quiz.questions.length;
-}
+$(document).on("click", "#start-over", game.reset.bind(game));
 
-function showScores() {
-	var gameOverhtml = "<h1>Result</h1>";
-	gameOverhtml += "<h2 'id=score'> Your scores: " + quiz.score + "</h2>";
-	var element = document.getElementById("quiz");
-	element.innerHTML = gameOverhtml;
-};
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked.bind(game, e)();
+});
 
-var questions = {
-	new Quesiton("Who was  the first president from Ohio?", ["William Henry Harrison", "John Quincy Adams", "George Washington", "John Tyler"], "William Henry Harrison"),
-	new Quesiton("When did Ohio become a State?", ["1826", "1856", "1815", "1803"], "1803"),
-	new Quesiton("Who was the first Governor of Ohio?", ["Ethan Allen Brown", "Edward Tiffin", "Jeremiah Morrow", "Wilson Shannon"], "Edward Tiffin"),
-	new Quesiton("How many United State Presidents have come from Ohio?", ["3", "5", "2", "8"], "8"),
-	new Quesiton("What is the name of the oldest city in Ohio", ["Cleveland", "Columbus", "Martins Ferry", "Cincinnati"], "Martins Ferry"),
-};
-
-var quiz = new Quiz(questions);
-
-populate();
-
-//timer
-document.getElementById('timer').innerHTML =
-  03 + ":" + 00;
-startTimer();
-
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
-}
-
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec};
-  if (sec < 0) {sec = "59"};
-  return sec;
-}
-
-
-
-
-//questions that will be answered and choices for those answers
-function Quiz(questions) {
-	this.score = 0;
-	this.questions = questions;
-	this.questionsIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-	return this.quesitons[this.questionIndex];
-}
-
-Quiz.prototype.isEnded = function() {
-	return this.questions.length === this.questionIndex
-}
-
-Quiz.prototype.guess = function(answer) {
-	if(this.getQuestionIndex().correctAnswer(answer)) {
-		this.scope++;
-	}
-
-	this.questionIndex++;
-};
-
-
-//quiz control to the right and wrong answers
-function Question(text, choices, answer) {
-	this.text = text;
-	this.choices = choices;
-	this.answer = answer;
-};
-
-Question.prototype.correctAnswer = function(choice) {
-	return choice === this.answer;
-}
-
+$(document).on("click", "#start", function() {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  game.loadQuestion.bind(game)();
+});
